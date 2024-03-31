76ff0c715e7d8f8ba7cbd3da7333cc35402a605b
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2024-03-31 18:43:07 +0300

    ComboBox

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem s = (ComboBoxItem)CMBX.SelectedItem;
+            string c = s.Content.ToString();
+            switch (c)
+            {
+                case "Красный":
+                    this.Background = Brushes.Red;
+                    break;
+                case "Синий":
+                    this.Background = Brushes.Blue;
+                    break;
+                case "Жёлтый":
+                    this.Background = Brushes.Yellow;
+                    break;
+                case "Зелёный":
+                    this.Background = Brushes.Green;
+                    break;
+            }
+        }
+
+        private void CMBX_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //ComboBoxItem s = (ComboBoxItem)CMBX.SelectedItem;
+            //string c = s.Content.ToString();
+            //switch (c)
+            //{
+            //    case "Красный":
+            //        this.Background = Brushes.Red;
+            //        break;
+            //    case "Синий":
+            //        this.Background = Brushes.Blue;
+            //        break;
+            //    case "Жёлтый":
+            //        this.Background = Brushes.Yellow;
+            //        break;
+            //    case "Зелёный":
+            //        this.Background = Brushes.Green;
+            //        break;
+            //}
+        }
+    }
+}
